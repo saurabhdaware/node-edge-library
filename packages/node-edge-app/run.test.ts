459308b5d8d520c638208086a04cf2018ba5d217
saurabhdaware <saurabhdaware99@gmail.com>
2023-12-01 23:47:11 +0530

    add comment

--- packages/node-edge-app/run.test.ts
+++ packages/node-edge-app/run.test.ts
@@ -3,6 +3,7 @@ import { getData } from "node-edge-library";
 
 describe('run', () => {
   test('should return edge true and node false', () => {
+    // You can also try running `VITEST_CONDITIONS=node vitest run` command to see this test fail
     const { isNode, isEdge } = getData();
     expect(isNode).toBe(false);
     expect(isEdge).toBe(true);
